cbca3e9af2d9ada685c55abbd4582d7e04f28285
Kasper Guldmann Nielsen <kalleguld@gmail.com>
2018-11-15 16:57:29 +0100

    Add project files.

---
+++ NinjectTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NinjectTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
